43d6d5b72772871f88fc0fec62c8b0a51846bcfc
Aman <aman.mishra.2005@gmail.com>
2024-05-16 18:02:54 +0530

    Added custom logger winston

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
+import logger from "./configs/logger.js";
 
 //env variables
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server is Listening on PORT", PORT);
+  logger.info("Server is listening on PORT", PORT);
 });
